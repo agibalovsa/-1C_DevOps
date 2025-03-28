ea4462f82acd3cf0c48d2d4976d207c02d395b23
agibalovsa <agibalovsa@gmail.com>
2025-03-28 17:51:20 +0400

    Create docker-build.sh for sonarqube

---
+++ docker/sonarqube/build/docker-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+source .arg
+
+DOCKER_BUILDKIT=1 docker build \
+  --build-arg "SONARQUBE_VERSION=${SONARQUBE_VERSION}" \
+  --build-arg "RUSSIAN_PACK=$RUSSIAN_PACK" \
+  --build-arg "BRANCH_PLUGIN_VERSION=$BRANCH_PLUGIN_VERSION" \
+  --build-arg "BSL_PLUGIN_VERSION=$BSL_PLUGIN_VERSION" \
+  -t sonarqube/1c:${SONARQUBE_VERSION}-${BSL_PLUGIN_VERSION} \
+  .
