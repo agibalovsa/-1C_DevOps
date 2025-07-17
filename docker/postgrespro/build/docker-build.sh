8b236a6d91cf3c0b16f3e7f02923058a391c27b2
Sergey Agibalov <agibalovsa@gmail.com>
2025-07-17 12:09:39 +0300

    Выполнены задачи: - Исправлен и унифицирован dockerfile для postgrespro.

--- docker/postgrespro/build/docker-build.sh
+++ docker/postgrespro/build/docker-build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+echo ${PG_MAJOR_VERSION}
+
 docker build \
     --build-arg "REGISTRY=${REGISTRY}" \
     --build-arg "OS_TAG=${OS_TAG}" \
-    --build-arg "PG_MAJOR=${PG_MAJOR}" \
+    --build-arg "PG_MAJOR_VERSION=${PG_MAJOR_VERSION}" \
+    --build-arg "PG_VERSION=${PG_VERSION}" \
+    --build-context context=context \
+    --build-context "context_arg=${CONTEXT_ARG}" \
     -t "${REGISTRY}${PG_TAG}" \
     .
 
