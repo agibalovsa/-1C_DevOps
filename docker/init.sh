26a744bbeb5a5c3dc25adf4da424d9f3ea4cecc7
Sergey Agibalov <agibalovsa@gmail.com>
2025-07-17 17:13:07 +0300

    Выполнены задачи: - Расширен файл init.sh для разворачивания compose. - Добавлен compose файл для slc.

--- docker/init.sh
+++ docker/init.sh
@@ -1,66 +1,157 @@
 #!/usr/bin/env bash
 
 # Initialisation dockers stack building script
-#  $1 - Сomma-separated сertificates URL
 #
-init_build_stack () {
+init_stack () {
 
     local -n return_name=$1
-    local -n return_paths=$2
+    local -n return_type=$2
+    local -n return_paths=$3
 
     name=$(\
         dialog --ascii-lines --no-shadow --erase-on-exit --output-fd 1 --title "Enter docker build stack" \
             --inputbox "" 8 40 \
     )
 
+    type=$(\
+        dialog --ascii-lines --no-shadow --no-tags --erase-on-exit --output-fd 1 --radiolist "Please select type" 0 0 0 \
+            "build" "build" off \
+            "compose" "compose" off
+    )
+
     IFS=" " read -ra paths <<< "$(
         dialog --ascii-lines --no-shadow --no-tags --erase-on-exit --output-fd 1 --radiolist "Please select applications" 0 0 0 \
-            "os/linux_deb/build" "os-system" off \
-            "imagick/build" "image magick" off \
-            "postgrespro/build" "image magick" off \
-            "jdk/build" "jdk" off \
-            "1c/build" "1c platform" off \
-            "1cesb/build" "1c-esb" off \
-            "1cans/build" "1c-ans" off \
-            "slc/build" "slc" off \
-            "hasp/build" "hasp" off \
-            "step-ca-cli/build" "step ca (cli)" off
+            "os/linux_deb/$type" "os-system" off \
+            "imagick/$type" "image magick" off \
+            "postgrespro/$type" "image magick" off \
+            "jdk/$type" "jdk" off \
+            "1c/$type" "1c platform" off \
+            "1cesb/$type" "1c-esb" off \
+            "1cans/$type" "1c-ans" off \
+            "slc/$type" "slc" off \
+            "hasp/$type" "hasp" off \
+            "step-ca-cli/$type" "step ca (cli)" off
     )"
 
     # shellcheck disable=SC2034
     return_name="${name}"
     # shellcheck disable=SC2034
+    return_type="${type}"
+    # shellcheck disable=SC2034
     return_paths=( "${paths[@]}" )
 
 }
 
-# Creating dockers stack building script
-#  $1 - Build stack name
-#  $2 - Build stack applications
+# Creating dockers stack compose script
+#  $1 - Stack name
+#  $2 - Stack applications
 #
-make_build_stack () {
+make_compose_stack () {
+
+    stack_path="users/compose/${1}"
+
+    if [ ! -d "${stack_path}" ]; then
+        mkdir -p "${stack_path}"
+    fi
 
     script_dir=$(dirname "$(readlink -f "$0")")
 
-    if [ ! -d user_builds ]; then
-        mkdir user_builds
+    set -a
+
+    if [ -f "${stack_path}/.env" ]; then
+        # shellcheck disable=SC1091
+        # shellcheck disable=SC1090
+        source "${stack_path}/.env"
+        rm "${stack_path}/.env"
     fi
 
-    if [ ! -d "user_builds/${1}" ]; then
-        mkdir "user_builds/${1}"
+    if [ -f "${stack_path}/docker-compose.sh" ]; then
+        rm "${stack_path}/docker-compose.sh"
     fi
+
+    printf "%s\n" \
+        "#!/bin/bash" \
+        "" \
+        "set -Eeoa pipefail" \
+        "" \
+        "script_dir=\$(dirname \"\$(readlink -f \"\$0\")\")" \
+        "" \
+        "# make context file" \
+        "mkdir -p \"\${script_dir}/context\"" \
+        "CONTEXT_ENV=\$(realpath \"\${script_dir}/context\")" \
+        "{" \
+            "echo \"CONTEXT_ENV=\${CONTEXT_ENV}\"" \
+            "echo \"\"" \
+            "cat \"\${script_dir}/../common_context/.env\"" \
+            "echo \"\"" \
+            "cat \"\${script_dir}/.env\"" \
+        "} >> \"\${CONTEXT_ENV}/.env\"" \
+        "" \
+        | tee "${stack_path}/docker-compose.sh.tmp" > /dev/null
+
+    make_docker_compose=0
+
+    paths=( "${!2}" )
+    for path in "${paths[@]}"
+    do
+        if [ -f "${script_dir}/${path}/docker-compose.yml" ]; then
+            {
+                echo "cd \"\${CONTEXT_ENV}\""
+                echo "cp ${script_dir}/${path}/docker-compose.yml \"\${CONTEXT_ENV}/docker-compose.yml\""
+                echo "docker compose up -d"
+            } >> "${stack_path}/docker-compose.sh.tmp"
+            make_docker_compose=1
+        fi
+        {
+            echo "# ${path}"
+            echo ""
+            envsubst < "${path}/.env.tmpl"
+            echo ""
+        } >> "${stack_path}/.env.tmp"
+    done
+
+    set +a
+
+    sed -i 's/{{/{/g' "${stack_path}/.env.tmp"
+    sed -i 's/}}/}/g' "${stack_path}/.env.tmp"
+
+    mv "${stack_path}/.env.tmp" "${stack_path}/.env"
+
+    if [ ${make_docker_compose} = 1 ]; then
+        echo "rm -r \"\${CONTEXT_ENV}\"" >> "${stack_path}/docker-compose.sh.tmp"
+        mv "${stack_path}/docker-compose.sh.tmp" "${stack_path}/docker-compose.sh"
+        chmod 744 "${stack_path}/docker-compose.sh"
+    else
+        rm "${stack_path}/docker-compose.sh.tmp"
+    fi
+
+}
+
+# Creating dockers stack building script
+#  $1 - Stack name
+#  $2 - Stack applications
+#
+make_build_stack () {
+
+    stack_path="users/builds/${1}"
+
+    if [ ! -d "${stack_path}" ]; then
+        mkdir -p "${stack_path}"
+    fi
+
+    script_dir=$(dirname "$(readlink -f "$0")")
 
     set -a
 
-    if [ -f "user_builds/${1}/.arg" ]; then
+    if [ -f "${stack_path}/.arg" ]; then
         # shellcheck disable=SC1091
         # shellcheck disable=SC1090
-        source "user_builds/${1}/.arg"
-        rm "user_builds/${1}/.arg"
+        source "${stack_path}/.arg"
+        rm "${stack_path}/.arg"
     fi
 
-    if [ -f "user_builds/${1}/docker-build.sh" ]; then
-        rm "user_builds/${1}/docker-build.sh"
+    if [ -f "${stack_path}/docker-build.sh" ]; then
+        rm "${stack_path}/docker-build.sh"
     fi
 
     printf "%s\n" \
@@ -80,10 +171,11 @@ make_build_stack () {
             "echo \"\"" \
             "cat \"\${script_dir}/.arg\"" \
         "} >> \"\${CONTEXT_ARG}/.arg\"" \
+        "" \
         "# shellcheck source=/dev/null" \
         "source \"\${CONTEXT_ARG}/.arg\"" \
         "" \
-        | tee "user_builds/${1}/docker-build.sh.tmp" > /dev/null
+        | tee "${stack_path}/docker-build.sh.tmp" > /dev/null
 
     make_docker_build=0
 
@@ -95,7 +187,7 @@ make_build_stack () {
                 echo "cd ${script_dir}/${path}"
                 echo "${script_dir}/${path}/docker-build.sh \"\$1\""
                 echo ""
-            } >> "user_builds/${1}/docker-build.sh.tmp"
+            } >> "${stack_path}/docker-build.sh.tmp"
             make_docker_build=1
         fi
         {
@@ -103,22 +195,22 @@ make_build_stack () {
             echo ""
             envsubst < "${path}/.arg.tmpl"
             echo ""
-        } >> "user_builds/${1}/.arg.tmp"
+        } >> "${stack_path}/.arg.tmp"
     done
 
     set +a
 
-    sed -i 's/{{/{/g' "user_builds/${1}/.arg.tmp"
-    sed -i 's/}}/}/g' "user_builds/${1}/.arg.tmp"
+    sed -i 's/{{/{/g' "${stack_path}/.arg.tmp"
+    sed -i 's/}}/}/g' "${stack_path}/.arg.tmp"
 
-    mv "user_builds/${1}/.arg.tmp" "user_builds/${1}/.arg"
+    mv "${stack_path}/.arg.tmp" "${stack_path}/.arg"
 
     if [ ${make_docker_build} = 1 ]; then
-        echo "rm -r \"\${CONTEXT_ARG}\"" >> "user_builds/${1}/docker-build.sh.tmp"
-        mv "user_builds/${1}/docker-build.sh.tmp" "user_builds/${1}/docker-build.sh"
-        chmod 744 "user_builds/${1}/docker-build.sh"
+        echo "rm -r \"\${CONTEXT_ARG}\"" >> "${stack_path}/docker-build.sh.tmp"
+        mv "${stack_path}/docker-build.sh.tmp" "${stack_path}/docker-build.sh"
+        chmod 744 "${stack_path}/docker-build.sh"
     else
-        rm "user_builds/${1}/docker-build.sh.tmp"
+        rm "${stack_path}/docker-build.sh.tmp"
     fi
 
 }
@@ -127,11 +219,22 @@ set -Eeo pipefail
 
 paths=( "common_context" )
 make_build_stack "common_context" paths[@]
+make_compose_stack "common_context" paths[@]
 
 declare name_stack
+declare type_stack
 # shellcheck disable=SC2034
 declare -a paths_stack
 
-init_build_stack name_stack paths_stack
+init_stack name_stack type_stack paths_stack
 
-make_build_stack "${name_stack}" paths_stack[@]
+if [ -z "${name_stack}" ] || [ -z "${type_stack}" ] || [ "${#paths_stack[@]}" == 0 ]; then
+    echo "Empty entries"
+    exit 1
+fi;
+
+if [ "${type_stack}" = "compose" ]; then
+    make_compose_stack "${name_stack}" paths_stack[@]
+else
+    make_build_stack "${name_stack}" paths_stack[@]
+fi;
