11a1b7a96337b2aefa8722965b664b95549c2212
Sergey Agibalov <agibalovsa@gmail.com>
2025-10-22 23:57:46 +0300

    Выполнены задачи: - Для сборки imagemagick добавлен параметр получения пакетов. Исправлены ошибки: - Ошибка сборки imagemagick. - Ошибка запуска контейнера portainer.

--- docker/imagick/build/docker-build.sh
+++ docker/imagick/build/docker-build.sh
@@ -14,6 +14,21 @@ docker build \
     -t "${REGISTRY}${IM_TAG}" \
     .
 
-if [ "${1}" = "push" ] && [ -n "${REGISTRY}" ]; then
+if [ "${1}" = "deb" ]; then
+docker build \
+    --build-arg "REGISTRY=${REGISTRY}" \
+    --build-arg "OS_TAG=${OS_TAG}" \
+    --build-arg "IM_VERSION=${IM_VERSION}" \
+    --build-arg "AOM_VERSION=${AOM_VERSION}" \
+    --build-arg "HEIF_VERSION=${HEIF_VERSION}" \
+    --build-arg "JXL_VERSION=${JXL_VERSION}" \
+    --build-arg "IMEI_VERSION=${IMEI_VERSION}" \
+    --build-context context=context \
+    --build-context common_context=../../common_context/build \
+    --build-context "context_arg=${CONTEXT_ARG}" \
+    --target deb \
+    --output deb \
+    .
+elif [ "${1}" = "push" ] && [ -n "${REGISTRY}" ]; then
     docker push "${REGISTRY}${IM_TAG}"
 fi
