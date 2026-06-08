e7010fccf81b0310d5929d7777c63cb0b4ebaa53
Sergey Agibalov <agibalovsa@gmail.com>
2026-06-08 17:23:22 +0300

    Исправлены ошибки: - Порядок ключей в команде step ca renew.

--- docker/step-ca-cli/build/entrypoint.sh
+++ docker/step-ca-cli/build/entrypoint.sh
@@ -41,12 +41,13 @@ setup_step_ca_certificate() {
 
     if [ ! -f "${CERT_PATH}/${DOMAIN_NAME}.crt" ]; then
         STEP_CA_CERT=( step ca certificate )
-        STEP_CA_CERT+=( --force --provisioner acme "${DOMAIN_NAME}" )
+        STEP_CA_CERT+=( --force )
         if ss -tulnp | grep ":80" ; then
             STEP_CA_CERT+=( --webroot=${WEB_ROOT} )
         else
             STEP_CA_CERT+=( --standalone )
         fi;
+        STEP_CA_CERT+=( --provisioner acme "${DOMAIN_NAME}" )
         STEP_CA_CERT+=( "${CERT_PATH}/${DOMAIN_NAME}.crt" "${CERT_PATH}/${DOMAIN_NAME}.key" )
 
         exec "${STEP_CA_CERT[@]}" 2>&1
